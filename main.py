8e11ea5c857ba7ac2da86ea12f429e7911a91a59
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-02-23 15:54:32 +0800

    getpass patch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from script import *
+import getpass
 
 MAIL_ADDRESS =  input("Email: ")
-PASSWORD = input("Password: ")
+PASSWORD = getpass.getpass()
 meet_link = input("Meet link: ")
 start(meet_link, MAIL_ADDRESS, PASSWORD)
